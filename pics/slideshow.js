ae26fccf3e97f11e3f412f2eac24160acc72b0a1
Vivek Soundararaj <vivegau@gmail.com>
2020-07-05 11:52:34 -0400

    Removing Unwanted stuff and adding 500px link...

--- pics/slideshow.js
+++ pics/slideshow.js
@@ -26,11 +26,9 @@ document.getElementById("slide1").appendChild(elem);
 
 var slideIndex = 1;
 showSlides(slideIndex);
-//getExif("photo" + slideIndex);
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
-  //getExif("photo" + slideIndex);
 }
 
 function currentSlide(n) {
@@ -41,41 +39,10 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var texts = document.getElementsByClassName("text");
-  if (n > slides.length) {slideIndex = 1}    
+  if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }  
+      slides[i].style.display = "none";
+  }
   slides[slideIndex-1].style.display = "block";
 }
-
-function getExif(ide) {
-    var img1 = document.getElementById(ide);
-    EXIF.getData(img1, function() {
-        var make = convertToFraction(EXIF.getTag(this, "ExposureTime"));
-        var model = EXIF.getTag(this, "FNumber");
-        var text = document.getElementById("text1");
-        text.innerHTML = "Exposure Time " + `${make}`  + " secs, F-Stop " + `${model}`;
-    });
-}
-
-function convertToFraction(x){
-  function highestCommonFactor(a,b) {
-    if (b==0) return a;
-    return highestCommonFactor(b,a%b);
-  }
-  var decimalArray = x.toString().split(".");
-  var leftDecimalPart = decimalArray[0];
-  var rightDecimalPart = decimalArray[1];
-  var numerator = leftDecimalPart + rightDecimalPart;
-  var denominator = Math.pow(10,rightDecimalPart.length); 
-  var factor = highestCommonFactor(numerator, denominator);
-  denominator /= factor;
-  numerator /= factor;
-  return numerator + "/" + denominator;
-}
-/*
-window.onload = function () {
-  getExif("photo1");
-};
-*/
